e4489b9373bb1154b66cb1c46d1a77be2ab5eb5b
LachowskiTomekPL <lachowskitomasz.pl@gmail.com>
2021-06-25 22:33:46 +0200

    creating Service,Controller, Repository

---
+++ src/main/java/com/github/lachowskitomekpl/todoapp/controller/TaskController.java
@@ -0,0 +1,2 @@
+package com.github.lachowskitomekpl.todoapp.controller;public class TaskController {
+}
---
+++ src/main/java/com/github/lachowskitomekpl/todoapp/service/TaskService.java
@@ -0,0 +1,2 @@
+package com.github.lachowskitomekpl.todoapp.service;public class TaskService {
+}
